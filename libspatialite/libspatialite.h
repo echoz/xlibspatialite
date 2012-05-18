11ccddd2796f97bc9e87eb18f3455b43e13a3a76
Jeremy Foo <jeremyfoo@gmail.com>
2012-05-19 04:19:53 +0800

    Initial Commit

---
+++ libspatialite/libspatialite.h
@@ -0,0 +1,13 @@
+//
+//  libspatialite.h
+//  libspatialite
+//
+//  Created by Jeremy Foo on 19/5/12.
+//  Copyright (c) 2012 ORNYX. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libspatialite : NSObject
+
+@end
